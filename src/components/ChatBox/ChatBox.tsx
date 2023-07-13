dde0515327a591de9d4dd2ff24dcc1a5483e46ec
MannySanz <mannysanz715@gmail.com>
2023-07-13 16:02:28 -0700

    Create and style chatbox

---
+++ src/components/ChatBox/ChatBox.tsx
@@ -0,0 +1,31 @@
+import { OpenAIMessage } from "@/pages/chat";
+
+type propsType = {
+  messages : OpenAIMessage[]
+}
+
+
+const ChatBox = ( {messages} : propsType ) => {
+  const userClasses = {
+    div : 'w-fill flex justify-end',
+    p : 'font-semibold orange-bubble p-3.5 rounded-3xl w-fit max-w-md h-fit'
+  }
+  const assistantClasses = {
+    div : 'w-fill flex justify-start',
+    p : 'font-semibold green-bubble p-8  rounded-3xl w-fit max-w-md h-fit'
+  }
+
+  return ( 
+    <div className="flex gap-10 flex-col pr-10 grow">
+      {messages && messages.map((message, idx) =>{
+        return( 
+          <div key={idx} className={message.role == 'user' ? userClasses.div : assistantClasses.div}>
+            <p className={message.role == 'user' ? userClasses.p : assistantClasses.p}>{message.content}</p>
+          </div>
+        )}
+      )}
+    </div>
+  );
+}
+
+export default ChatBox;
