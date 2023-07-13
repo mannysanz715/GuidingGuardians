76088f40b8db5773bca0cb80994b1ca39d6aff06
MannySanz <mannysanz715@gmail.com>
2023-07-13 14:41:13 -0700

    Hide Scroll Bar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -4,7 +4,7 @@ import Link from "next/link"
 
 const NavBar = () =>{
   return(
-    <div className="flex justify-between my-6 mx-12 items-center">
+    <div className=" flex justify-between my-6 mx-12 items-center">
       <img src="/assets/logo.png" alt="GuidingGuardians"/>
       <ul className="right-section-nav text-2xl flex items-center gap-7">
         <Link href='/about'><li className='hover:bg-slate-100 hover:text-slate-500 hover:rounded focus: p-3 about-button'>About</li></Link>
