ba88c7f587e99b5e0b5cf94455630687fd42570e
dkobrin <shdwkat999@yahoo.com>
2023-07-13 13:55:56 -0400

    fixed about link

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -7,7 +7,7 @@ const NavBar = () =>{
     <div className="flex justify-between my-6 mx-12 items-center">
       <img src="/assets/logo.png" alt="GuidingGuardians"/>
       <ul className="right-section-nav text-2xl flex items-center gap-7">
-        <Link href='#'><li className='hover:bg-slate-100 hover:text-slate-500 hover:rounded focus: p-3 about-button'>About</li></Link>
+        <Link href='/about'><li className='hover:bg-slate-100 hover:text-slate-500 hover:rounded focus: p-3 about-button'>About</li></Link>
         <Link href="#"><li className='hover:bg-slate-100 hover:rounded hover:text-slate-100 sign-in-button border-solid border-2  rounded-3xl p-3'>Sign-in</li></Link>
       </ul>
     </div>
