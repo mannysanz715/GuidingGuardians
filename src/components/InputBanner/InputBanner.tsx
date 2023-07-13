a32ca0b05909a4be43ef2ad6332cd058c9bc30bb
MannySanz <mannysanz715@gmail.com>
2023-07-13 14:25:46 -0700

    Create Chat History box and style

--- src/components/InputBanner/InputBanner.tsx
+++ src/components/InputBanner/InputBanner.tsx
@@ -1,22 +1,34 @@
 /* eslint-disable @next/next/no-img-element */
-import { useState } from "react"
+import Link from "next/link"
+import { use, useState } from "react"
 
 const InputBanner = () => {
-const [hasText, setHasText] = useState(false)
+  const [hasText, setHasText] = useState(false)
+
+  const [inputString, setInputString] = useState('')
+
   function onChange(e:any){
+    setInputString(e.target.value)
     if(e.target.value){
       setHasText(true)
     }
+    else setHasText(false)
   }
+
   return (
     <div className="flex h-64 flex-col justify-center gap-10 items-center background-color">
       <h1 className="font-bold text-5xl text-white">Ask Anything!</h1>
       <div className="input-container-width flex items-center justify-between pl-5 border rounded-3xl bg-white input-container">
         <img src="/assets/searchIcon.png" alt="" className="h-5 w-5" />
         <input autoCorrect="off" onChange={onChange} autoComplete="off" type="text" className="input-width focus:outline-none h-9 px-3 rounded-2xl"/>
-        <button className="m-0"> <img className=" m-0 rounded-r-3xl " src={hasText ? "/assets/submitArrow.jpg" : "/assets/greySubmit.jpg"} alt="" /></button>
+        
+        <Link href={{
+          pathname: '/chat',
+          query: {
+          data :`explain without giving the answer ${inputString}`
+          }
+        }}> <img className=" m-0 rounded-r-3xl " src={hasText ? "/assets/submitArrow.jpg" : "/assets/greySubmit.jpg"} alt="" /></Link>
       </div>
-      
     </div>
 )
 }
---
+++ src/pages/chat.tsx
@@ -0,0 +1,114 @@
+import { useRouter } from "next/router";
+import { AxiosConfigOptions, HTTPMethods, RequestOptions, ResponseObj } from "@/types/externalApiCall";
+import { ChangeEvent, MouseEvent, useEffect, useState } from "react";
+import axios from "axios";
+import NavBar from "@/components/NavBar/NavBar";
+import SubBanner from "@/components/SubBanner/SubBanner";
+import ChatHistoryBox from "@/components/ChatHistoryBox/ChatHistoryBox";
+
+type OpenAIMessage = {
+  role: string,
+  content: string,
+};
+
+type OpenAIResponseChoice = {
+  finish_reason: string,
+  index: number,
+  message: OpenAIMessage,
+}
+
+type OpenAIResponseT = {
+  choices: OpenAIResponseChoice[],
+  id: string,
+  object: string,
+  created: number,
+  model: string,
+  usage:{ 
+      completion_tokens: number,
+      prompt_tokens: number,
+      total_tokens: number,
+  },   
+}
+
+const Chat = () => {
+  useEffect(()=>{
+    const temp = [...msgHistory];
+    apiCommunication()
+  }, [])
+
+  const router = useRouter()
+  const { data } = router.query
+  const stringData = data as string
+  const [userInput, setUserInput] = useState(stringData)
+
+  const initialMessage = {
+    role: 'user',
+    content: userInput,
+  }
+
+  //* STATE SETUP
+  const [msgHistory, setMsgHistory] = useState([initialMessage])
+
+  const handleChange = (e: ChangeEvent) => {
+    const target = e.target as HTMLInputElement;
+    setUserInput(target.value);
+  }
+
+  const handleSubmit = async (e: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>) => {
+    e.preventDefault();
+    const temp = [...msgHistory];
+    
+    console.log("chat - handleSubmit",{temp})
+  }
+
+  const apiCommunication = async () => {
+    const temp = [...msgHistory]
+    console.log("chat userinput - apiCommunication",{userInput})
+    const resp = await hydrate(userInput);
+    if (resp) {
+      temp.push(resp.choices[0].message);
+    }
+    setMsgHistory(temp);
+    console.log("chat - apiCommunication",{temp})
+    setUserInput('')
+    return await new Promise<OpenAIResponseT>((resolve) => {
+      resolve(resp)
+    });
+  }
+
+  const hydrate = async (content: string) :Promise<OpenAIResponseT> => {
+    const config: RequestOptions = {
+        [AxiosConfigOptions.url]: "/api/openai_completion",
+        [AxiosConfigOptions.method]: HTTPMethods.POST,
+        [AxiosConfigOptions.data]: [{
+                "role": "user",
+                "content": content
+        }]
+    }
+    const res = await axios<ResponseObj>(config);
+    const {data, error} = res.data;
+    // if(!error) {
+        const resp = data as OpenAIResponseT;
+        return await new Promise<OpenAIResponseT>((resolve) => {
+            resolve(resp)
+        });
+    // } 
+    // return null;
+    
+  }
+
+  console.log(data)
+  return ( 
+    <div>
+      <NavBar />
+      <div className="flex ml-12 chat-box-height">
+        <ChatHistoryBox />
+    
+
+      </div>
+      
+    </div>
+  );
+}
+
+export default Chat;
