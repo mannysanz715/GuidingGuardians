67296bf68b827a52f428ff2c0653b0347b0cb7ca
dkobrin <shdwkat999@yahoo.com>
2023-07-13 22:40:48 -0400

    input field on chat page functional

--- src/components/ChatBoxInput/ChatBoxInput.tsx
+++ src/components/ChatBoxInput/ChatBoxInput.tsx
@@ -1,7 +1,15 @@
-const ChatBoxInput = () => {
+import { ChangeEvent, FormEvent, MouseEvent } from "react";
+
+interface propsInt {
+  value: string;
+  updateFunc: (e: ChangeEvent) => void;
+  submitFunc: (e: FormEvent<HTMLFormElement>) => Promise<void>;
+}
+
+const ChatBoxInput = ({value, updateFunc, submitFunc} : propsInt) => {
   return ( 
-    <form className="w-100 flex justify-center items-center ">
-      <input className="input-background-color w-5/6 py-2 pl-5 rounded-3xl" placeholder="Ask another question..."/>
+    <form className="w-100 flex justify-center items-center " onSubmit={(e) => submitFunc(e)}>
+      <input value={value} className="input-background-color w-5/6 py-2 pl-5 rounded-3xl" onChange={(e) => updateFunc(e)} placeholder="Ask another question..."/>
     </form>
   );
 }
